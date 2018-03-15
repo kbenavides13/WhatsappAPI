f5bdcaa693a7104ea014b293444d71ea324a13b9
kenneth <kenneth@PC-BF.5kh3b35xgjsetfoj5xahqpnz1d.bx.internal.cloudapp.net>
2018-03-14 23:16:47 -0600

    Agregado lo que se consiguio de otro repositorio

---
+++ WhatsappAPI/src/__init__.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "kenneth"
+__date__ = "$Mar 14, 2018 11:15:03 PM$"
+
+if __name__ == "__main__":
+    print "Hello World"
